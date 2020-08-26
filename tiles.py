0f58888b772062e2f506f0cb4441e77a0443165c
Benjamin Casey <benjamincasey55@gmail.com>
2020-08-27 02:55:55 +1000

    Added two demo tiles to the tile list

---
+++ tiles.py
@@ -0,0 +1,7 @@
+# List of world tiles
+from world_tile import Tile
+
+tiles = [
+    Tile('Grassy footland', [0, 0], 'A grassy footland. This is where you awoke. There are plains to the north'),
+    Tile('Plains', [0, 1], 'A plain field. This is due north of where you started.')
+]
